5372815ac60c86025c1fcd04e019a04b8915ee89
Alex Santiago Perez Illera <alexperez@unimayor.edu.co>
2026-08-31 08:57:49 -0500

    Feat: primer cambio en la hoja de vida

--- src/hojadevida/modelo/DatosHojaVida.java
+++ src/hojadevida/modelo/DatosHojaVida.java
@@ -6,12 +6,12 @@ package hojadevida.modelo;
 public class DatosHojaVida {
 
     // ========== PERSONALIZA TUS DATOS AQUÍ ==========
-    private String nombre = "Tu Nombre Completo";
-    private String titulo = "Estudiante de Estructura de Datos";
-    private String ciudad = "Ciudad, País";
-    private String telefono = "+57 XXX XXX XXXX";
-    private String email = "tu@email.com";
-    private String github = "github.com/tu-usuario";
+    private String nombre = "Alex Santiago Pérez Illera";
+    private String titulo = "Tecnologia en desarrollo de software";
+    private String ciudad = "Popayán, Colombia";
+    private String telefono = "+57 321 737 7659";
+    private String email = "alexperez@unimayor.edu.co";
+    private String github = "github.com/ASPERI2026";
     private String[] habilidadesTecnicas = {"Java", "Git y GitHub", "Programación", "Estructura de Datos"};
     private String[] habilidadesBlandas = {"Trabajo en equipo", "Comunicación", "Resolución de problemas"};
     private String idiomas = "Español - Nativo\nEnglish - Básico (B2)";
